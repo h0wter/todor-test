be305e8683645eb9eecd86b4056769f08ac2c465
h0wter <91832203+h0wter@users.noreply.github.com>
2023-10-18 19:08:45 +0300

    tt-03: + not-found page

--- src/libs/components/router/router.tsx
+++ src/libs/components/router/router.tsx
@@ -2,11 +2,14 @@ import { Route } from 'react-router-dom';
 import { RouterProvider } from '../router-provider/router-provider';
 import { AppRoute } from '../../enums';
 import { PageLayout } from '../page-layout/page-layout';
+import { NotFound } from '../../pages';
 
 const Router = () => {
   return (
     <RouterProvider>
-      <Route path={AppRoute.ROOT} element={<PageLayout />} />
+      <Route path={AppRoute.ROOT} element={<PageLayout />}>
+        <Route path={AppRoute.ANY} element={<NotFound />} />
+      </Route>
     </RouterProvider>
   );
 };
