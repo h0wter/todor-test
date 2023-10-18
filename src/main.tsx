09cf9ddf25d0873cfc7de6da88e29d3896c1ce2a
h0wter <91832203+h0wter@users.noreply.github.com>
2023-10-18 15:37:59 +0300

    tt-01: * add router to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import './assets/css/styles.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { Router } from './libs/components';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
 );
